89729468d61d75e160291be6e6b072281e1a680f
luckysher <luckyshera95@gmail.com>
2018-03-26 14:04:51 +0530

    added bulma css link

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 echo "<html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css"><script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"><script>
     <title>
         PHP Demos
     </title>
@@ -16,7 +19,13 @@ include 'templates/footer/footer.php';
 echo "<!-- footer end -->";
 
 
-echo "PHP Demos";
+echo "
+<div class="columns">
+    <div class="columns is-half is-success">
+        column
+    </div>
+</div>
+";
 
 echo "</body>
 </html>";
