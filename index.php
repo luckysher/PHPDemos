21b68e4c737aa54a4e6f8365ade5ef70fb17eb47
luckysher <luckyshera95@gmail.com>
2018-03-26 14:28:47 +0530

    updated index and added some body content

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 echo "<html>
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css"><script>
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"><script>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css'/>
+    <script defer src='https://use.fontawesome.com/releases/v5.0.6/js/all.js'></script>
     <title>
         PHP Demos
     </title>
@@ -20,9 +20,11 @@ echo "<!-- footer end -->";
 
 
 echo "
-<div class="columns">
-    <div class="columns is-half is-success">
-        column
+<div class='container'>
+    <div class='columns hero is-success'>
+        <div class='column hero is-half is-warning'>
+            column
+        </div>
     </div>
 </div>
 ";
