11818401d5a11456343a163f65a05c604b4f58c4
luckysher <luckyshera95@gmail.com>
2018-03-26 14:37:12 +0530

    added body.php

---
+++ templates/body/body.php
@@ -0,0 +1,11 @@
+<?php
+echo "
+    <div class='container'>
+        <div class='columns hero is-success'>
+            <div class='column hero is-half is-warning'>
+                column
+            </div>
+        </div>
+    </div>
+";
+?>
