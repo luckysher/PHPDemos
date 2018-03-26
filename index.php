7531452d1479d601804cb45a9f823cb78d9314ec
luckysher <luckyshera95@gmail.com>
2018-03-26 14:37:46 +0530

    updated index.php for including body template

--- index.php
+++ index.php
@@ -1,36 +1,31 @@
 <?php
 echo "<html>
-<head>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css'/>
-    <script defer src='https://use.fontawesome.com/releases/v5.0.6/js/all.js'></script>
-    <title>
-        PHP Demos
-    </title>
-</head>
-<body>
-
-<!-- Header start -->";
-include 'templates/header/header.php';
-echo "<!-- Header end -->";
-
-echo "<!-- footer start -->";
-include 'templates/footer/footer.php';
-echo "<!-- footer end -->";
-
-
-echo "
-<div class='container'>
-    <div class='columns hero is-success'>
-        <div class='column hero is-half is-warning'>
-            column
-        </div>
-    </div>
-</div>
+    <head>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css'/>
+        <script defer src='https://use.fontawesome.com/releases/v5.0.6/js/all.js'></script>
+        <title>
+            PHP Demos
+        </title>
+    </head>
+    <body>
 ";
 
+echo "<!-- Header start -->";
+    include 'templates/header/header.php';
+echo "<!-- Header end -->";
+
+echo "<!-- body start -->";
+    include 'templates/body/body.php';
+echo "<!-- body end -->";
+
+echo "<!-- footer start -->";
+    include 'templates/footer/footer.php';
+echo "<!-- footer end -->";
+
 echo "</body>
-</html>";
+    </html>
+";
 ?>
 
 
