eb68ac63d7a72a17765db478bf2927bedd060204
luckysher <luckyshera95@gmail.com>
2018-03-27 12:15:18 +0530

    updated title importg

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ echo "<html>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css'/>
         <script defer src='https://use.fontawesome.com/releases/v5.0.6/js/all.js'></script>
         <title>
-           { $title }
+            $demo->title; 
         </title>
     </head>
     <body>
