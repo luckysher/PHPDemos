83b22c50078bff709beeffbf3e94cec05b20621b
luckysher <luckyshera95@gmail.com>
2018-03-27 12:15:52 +0530

    added class for demo data

--- templates/demo_data/data.php
+++ templates/demo_data/data.php
@@ -1,3 +1,17 @@
 <?php
-    $title = "PHP Demos";
+   class PHPDemos{
+        //  Demo vars
+        public $title;
+
+        public function __construct(){
+            $this->title = "PHP Demos";
+        }
+
+        public function setTitle($title){
+            $this->title = $title;
+        }
+
+   }
+
+   $demos = new PHPDemos;
 ?>
