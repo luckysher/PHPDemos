32b2645ce93a267f50bc2137dd7e6ee1321accda
luckysher <luckyshera95@gmail.com>
2018-03-28 14:39:36 +0530

    fixed banner

--- templates/body/body.php
+++ templates/body/body.php
@@ -1,13 +1,19 @@
 <?php
 echo '<div class="page">'.
-     '  <div class="banner hero is-primary">'.
-     '     <h3 class="title">PHP Demos</h3>'.
-     '     <h3 class="subtitle">Using bulma css framework</h3>'.
-     '  </div>'.
-     '  <div class="columns">'.
-     '       <div class="column">'.
+     '    <div class="banner hero-body is-primary">'.
+     '       <div class="hero-body container">'.
+     '          <div class="columns is-vcentered">'.
+     '             <div class="column">'.
+     '                 <h3 class="title">PHP Demos</h3>'.
+     '                 <h3 class="subtitle">Using bulma css framework</h3>'.
+     '              </div>'.
+     '           </div>'.
+     '        </div>'.
+     '    </div>'.
+     '    <div class="columns">'.
+     '        <div class="column">'.
      '           column'.
-     '       </div>'.
-     '   </div>'.
+     '        </div>'.
+     '     </div>'.
      '</div>';
 ?>
