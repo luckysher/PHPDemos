a9c64f0d9f48ee658c1dfd47ea2590ee914783ae
luckysher <luckyshera95@gmail.com>
2018-03-28 12:39:23 +0530

    added var for tabs

--- templates/demo_data/data.php
+++ templates/demo_data/data.php
@@ -3,10 +3,12 @@
         //  Demo vars
         public $title;
         public $menus;
+        public $tabs;
 
         public function __construct(){
             $this->title = "PHP Demos";
             $this->menus = ['Home', 'Contact us', 'About Us'];
+            $this->tabs = ['Form Demo', 'List Render demo', 'Modal Demo'];
         }
 
         public function setTitle($title){
