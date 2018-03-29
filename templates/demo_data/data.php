ab4595854512853d67ce782530c82ddc2cbb37df
luckysher <luckyshera95@gmail.com>
2018-03-30 04:28:21 +0530

    updated data.php and added placeholder values

--- templates/demo_data/data.php
+++ templates/demo_data/data.php
@@ -10,8 +10,13 @@
             $this->title = "PHP Demos";
             $this->menus = ['Home', 'Contact us', 'About Us'];
             $this->tabs = ['Form Demo', 'List Render demo', 'Modal Demo'];
-            $this->formFields = array('Name' => 'name', 'Age' => 'age', 'Marks' => 'marks', 'Roll no' => 'rnum');
-        }
+            //$this->formFields = array('Name' => 'name', 'Age' => 'age', 'Marks' => 'marks', 'Roll no' => 'rnum');
+            $this->formFields = [
+                        array('label' => 'Name', 'fname' => 'name', 'placeholder' => 'Enter your name here..' ),
+                        array('label' => 'Age', 'fname' => 'age', 'placeholder' => 'Enter your age here..' ),
+                        array('label' => 'Marks', 'fname' => 'marks', 'placeholder' => 'Enter your marks here..' ),
+                        array('label' => 'Roll no.', 'fname' => 'rnum', 'placeholder' => 'Enter your roll no. here..' )];
+           }
 
         public function setTitle($title){
             $this->title = $title;
