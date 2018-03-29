2a17bf1cc7879584bef4e1f17fd3cd47a475b6bc
luckysher <luckyshera95@gmail.com>
2018-03-29 19:14:54 +0530

    added method for rendering form fields

--- templates/demo_data/demoForm.php
+++ templates/demo_data/demoForm.php
@@ -3,12 +3,25 @@
     class DemoForm {
         // form extra fields
         public $formName;
+        // form id
+        public $fid;
 
          // constructor
          public __constructor(){
             $this-> formName = "Demo Form";
+            $this-> fid = "dform";
          }
 
+        public addFormFields(){
+            foreach($demos->formFields as $label => $fname){
+                  echo '<div class="field">'.
+                            <label class="label">$label</label>'.
+                            <div class="control">'.
+                                <input class="input" type="text" placeholder="Text input">'.
+                            </div>'.
+                        </div>';
+            }
+        }
 
     }
 ?>
