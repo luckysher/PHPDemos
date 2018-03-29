9a9292a7f41aef6acee6002d9f198a4de7504dc5
luckysher <luckyshera95@gmail.com>
2018-03-29 18:57:29 +0530

    added form class

---
+++ templates/demo_data/demoForm.php
@@ -0,0 +1,14 @@
+<?php
+
+    class DemoForm {
+        // form extra fields
+        public $formName;
+
+         // constructor
+         public __constructor(){
+            $this-> formName = "Demo Form";
+         }
+
+
+    }
+?>
