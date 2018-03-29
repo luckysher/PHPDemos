c4763a4509e70a3cd8a8f031870bca87297ed4e4
luckysher <luckyshera95@gmail.com>
2018-03-30 04:50:38 +0530

    added comments for modal

--- templates/modals/modal.php
+++ templates/modals/modal.php
@@ -7,18 +7,24 @@
         public $description;
         public $buttons;
 
+        // modal constructor with modal title param
         public function __construct($title){
             $this->title = $title;
         }
 
+        // method for setting modal body text
         public function setBodyText($txt){
             $this->description = $txt;
         }
 
+        // method for setting buttons names
         public function setButtons($btns){
             $this->buttons = $btns;
         }
 
+        // method for showing modal
+        public function show(){
 
+        }
     }
 ?>
