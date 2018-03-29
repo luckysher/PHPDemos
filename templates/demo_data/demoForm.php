0f2f6cff408884d31f770502a76327b995946a83
luckysher <luckyshera95@gmail.com>
2018-03-29 19:24:09 +0530

    added form start method

--- templates/demo_data/demoForm.php
+++ templates/demo_data/demoForm.php
@@ -11,17 +11,20 @@
             $this-> formName = "Demo Form";
             $this-> fid = "dform";
          }
-
+        public startForm(){
+            echo '<form id="$this->fid">';
+        }
         public addFormFields(){
             foreach($demos->formFields as $label => $fname){
                   echo '<div class="field">'.
-                            <label class="label">$label</label>'.
-                            <div class="control">'.
-                                <input class="input" type="text" placeholder="Text input">'.
-                            </div>'.
-                        </div>';
+                       '     <label class="label">$label</label>'.
+                       '     <div class="control">'.
+                       '         <input class="input" type="text" placeholder="Text input" name="$fname">'.
+                       '     </div>'.
+                       '</div>';
             }
         }
+
 
     }
 ?>
