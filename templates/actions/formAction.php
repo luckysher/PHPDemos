4ac00cbe177fb96e90c52ee325047ba6c781da05
luckysher <luckyshera95@gmail.com>
2018-03-31 15:20:42 +0530

    updated form action

--- templates/actions/formAction.php
+++ templates/actions/formAction.php
@@ -2,11 +2,11 @@
  if ($_POST){
         $modal = new Modal("Form Data Demo with Modal");
         $description = "modal description:".
-        foreach($_POST as $fname => $value){
-                       "   form field : ".$fname."  :  ".$value;.
-        }
-                        "".
-                        "";
+                       "   form fields : Name  :  ".$_POST["name"].
+                       "   form fields : Age  :  ".$_POST["age"].
+                       "   form fields : Marks  :  ".$_POST["marks"].
+                       "   form fields : Roll No  :  ".$_POST["rnum"];
+
         $modal->setBodyText($description);
         $modal->show();
  }
