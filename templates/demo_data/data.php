1c93b71d862c7fea54fcd3d0c2f76bc7d92ea24b
luckysher <luckyshera95@gmail.com>
2018-04-02 13:11:19 +0530

    added data fields for errors and val

--- templates/demo_data/data.php
+++ templates/demo_data/data.php
@@ -5,26 +5,16 @@
         public $menus;
         public $tabs;
         public $formFields;
-        //form data and error fileds
-
-        public $name_error = "";
-        public $age;
-        public $age_error = "";
-        public $marks;
-        public $marks_error="";
-        public $rnum;
-        public $rnum_error = "";
 
         public function __construct(){
             $this->title = "PHP Demos";
             $this->menus = ['Home', 'Contact us', 'About Us'];
             $this->tabs = ['Form Demo', 'List Render demo', 'Modal Demo'];
-            //$this->formFields = array('Name' => 'name', 'Age' => 'age', 'Marks' => 'marks', 'Roll no' => 'rnum');
             $this->formFields = [
-                        array('label' => 'Name', 'fname' => 'name', 'placeholder' => 'Enter your name here..' ),
-                        array('label' => 'Age', 'fname' => 'age', 'placeholder' => 'Enter your age here..' ),
-                        array('label' => 'Marks', 'fname' => 'marks', 'placeholder' => 'Enter your marks here..' ),
-                        array('label' => 'Roll no.', 'fname' => 'rnum', 'placeholder' => 'Enter your roll no. here..' )];
+                        array('label' => 'Name', 'fname' => 'name', 'placeholder' => 'Enter your name here..', 'val' => '', 'errors' => ''),
+                        array('label' => 'Age', 'fname' => 'age', 'placeholder' => 'Enter your age here..', 'val' => '', 'errors' => ''),
+                        array('label' => 'Marks', 'fname' => 'marks', 'placeholder' => 'Enter your marks here..', 'val' => '', 'errors' => ''),
+                        array('label' => 'Roll no.', 'fname' => 'rnum', 'placeholder' => 'Enter your roll no. here..', 'val' => '', 'errors' => '')];
            }
 
         public function setTitle($title){
