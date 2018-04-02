1317f7ef4525e996e0457dd9bbd6e388ecd5e5f8
luckysher <luckyshera95@gmail.com>
2018-04-02 14:31:26 +0530

    added comments for various check

--- templates/actions/formAction.php
+++ templates/actions/formAction.php
@@ -4,21 +4,25 @@
 // check the error for th form data here
 function checkFormErrors($demos){
     $errors = false;
+    //name check
     if(empty($_POST["name"])){
         $errors = True;
         $demos->formFields[0]["name"] = $_POST["name"];
         $demos->formFields[0]["errors"] = "Name should be non empty/valid  string";
     }
+    //age check
      if(empty($_POST["age"])){
         $errors = True;
         $demos->formFields[1]["val"] = $_POST["age"];
         $demos->formFields[1]["errors"] = "Age should be non empty/valid integer";
     }
+    //marks check
     if(empty($_POST["marks"])){
         $errors = True;
         $demos->formFields[2]["val"] = $_POST["marks"];
         $demos->formFields[2]["errors"] = "Marks is required";
     }
+    // roll num check
     if(empty($_POST["rnum"])){
         $errors = True;
         $demos->formFields[3]["val"] = $_POST["rnum"];
