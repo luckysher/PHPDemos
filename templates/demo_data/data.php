3d9ab4263864692eae22d33359639824bcb6dec2
luckysher <luckyshera95@gmail.com>
2018-04-02 14:12:03 +0530

    added comments for data class

--- templates/demo_data/data.php
+++ templates/demo_data/data.php
@@ -1,4 +1,7 @@
 <?php
+// Data class for PHP demo
+// set the various data here
+
    class PHPDemos{
         //  Demo vars
         public $title;
@@ -6,10 +9,13 @@
         public $tabs;
         public $formFields;
 
+        // constructor
         public function __construct(){
             $this->title = "PHP Demos";
             $this->menus = ['Home', 'Contact us', 'About Us'];
             $this->tabs = ['Form Demo', 'List Render demo', 'Modal Demo'];
+
+            // set the other fields with label etc here.
             $this->formFields = [
                         array('label' => 'Name', 'fname' => 'name', 'placeholder' => 'Enter your name here..', 'val' => '', 'errors' => ''),
                         array('label' => 'Age', 'fname' => 'age', 'placeholder' => 'Enter your age here..', 'val' => '', 'errors' => ''),
