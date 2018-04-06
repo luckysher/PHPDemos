7aa04f44f8f1ed3ec6d064e801059019a8e9ea77
luckysher <luckyshera95@gmail.com>
2018-04-06 23:10:13 +0530

    added check for modak demo

--- templates/body/body.php
+++ templates/body/body.php
@@ -42,7 +42,10 @@ echo '        <div class="columns">'.
                         echo '<!-- Demo List start here -->';
                         echo '<h1 style="color:red;">List Demo</h1>';
                         echo '<!-- Demo List end here -->';
-
+                   else if ($active === 'modal-demo'):
+                        echo '<!-- Demo modal start here -->';
+                        echo '<h1 style="color:green;">Modal Demo</h1>';
+                        echo '<!-- Demo modal end here -->';
                    endif;
 echo '           </div>'.
      '        </div>'.
