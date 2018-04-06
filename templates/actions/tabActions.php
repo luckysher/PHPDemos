0ec20ce2df9d9b97a977be210b0c37f78087d877
luckysher <luckyshera95@gmail.com>
2018-04-06 22:26:22 +0530

    updated tabAction

--- templates/actions/tabActions.php
+++ templates/actions/tabActions.php
@@ -1,17 +1,20 @@
 <?php
-  $form = false;
-  $modal = false;
-  $list = false;
+  $active = '';
+  //$form = false;
+  //$modal = false;
+  //$list = false;
 
   if($_GET):
-    if($_GET("show") === "#form"):
-        $form = true;
-    else if($_GET("show") === "#modal"):
-        $modal = true;
-    else if($_GET("show") === "#list"):
-        $list = true;
+    if(isset($_GET("show")):
+        $active = $_GET("show");
+     //=== "#form"):
+        //$form = true;
+    //    $active =
+    //else if($_GET("show") === "#modal"):
+    //    $modal = true;
+    //else if($_GET("show") === "#list"):
+    //    $list = true;
     endif;
   endif;
-
 
 ?>
