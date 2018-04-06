d000631a8fbfd67580cb8dcde682c7883f9bd9dd
luckysher <luckyshera95@gmail.com>
2018-04-06 13:57:34 +0530

    updated footer

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -1,8 +1,10 @@
 <?php
 
-echo "footer template";
-
-
+echo '<div class="columns hero is-black">'.
+     '  <div class="column is-vcentered">'.
+     '      <h3 class="is-centered is-text-white">demo@All Right reserved</h3>'.
+     '  </div>'.
+     '</div>';
 ?>
 
 
