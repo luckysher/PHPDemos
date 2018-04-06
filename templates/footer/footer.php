b839d2f6a90313443566e8d65feba0c194c60f8c
luckysher <luckyshera95@gmail.com>
2018-04-06 18:42:24 +0530

    updated footer alignement

--- templates/footer/footer.php
+++ templates/footer/footer.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<div class="columns hero is-black" style="height:20%;">'.
-     '  <div class="column is-vcentered">'.
+     '  <div class="column is-vcentered" style="display:flex;align-items:center;">'.
      '      <h3 class="has-text-centered is-text-white">demo@All Right reserved</h3>'.
      '  </div>'.
      '</div>';
